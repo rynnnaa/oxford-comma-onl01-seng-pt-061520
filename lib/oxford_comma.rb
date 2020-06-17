f148901e81ef3ddf0b3de164fdfee8bac4259bdc
Ryna Truong <rynatruong@outlook.com>
2020-06-17 15:54:13 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
-
+def oxford_comma(arr)
+  if arr.length == 1
+    arr.join(", ")
+    elsif arr.length == 2
+    arr.join(" and ")
+  else
+    element_popped = arr.pop
+    arr.push("and #{element_popped}")
+    arr.join(", ")
+  end
 end
